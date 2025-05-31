9efe3c171dd18524aece6bb289c7cf9e6115cf88
Jonas Heinle <jonasheinle@googlemail.com>
2025-05-31 23:15:56 +0200

    Update demo_py_spy.py

--- bench/demo_py_spy.py
+++ bench/demo_py_spy.py
@@ -5,6 +5,7 @@ from kataglyphispythonpackage.dummy import SimpleMLPreprocessor
 def main():
     preprocessor = SimpleMLPreprocessor(n_samples=1_000_000)
     result = preprocessor.run_pipeline()
+    time.sleep(100)
 
 if __name__ == "__main__":
     main()
