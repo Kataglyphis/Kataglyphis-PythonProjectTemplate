9718c7c3d995a969c67173280e0bc005fd384f45
Jonas Heinle <jonasheinle@googlemail.com>
2025-10-08 17:27:56 +0200

    incooperating uv

--- scripts/windows/setup-dependencies.ps1
+++ scripts/windows/setup-dependencies.ps1
@@ -10,8 +10,9 @@ $ErrorActionPreference = 'Stop'
 # Install LLVM/Clang via Chocolatey
 Write-Host "Installing LLVM/Clang $ClangVersion..."
 winget install --accept-source-agreements --accept-package-agreements --id=LLVM.LLVM -v $ClangVersion -e
-# get ninja
+# Get and install ninja
 Write-Host "Installing Ninja via winget..."
 winget install --accept-source-agreements --accept-package-agreements --id=Ninja-build.Ninja  -e
-
+# Install Astral UV
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 Write-Host "=== Dependency installation completed ==="
